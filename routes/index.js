f6c374144f90148f84a0c434b39bdaf811e1b63e
jiangfei03 <jiangfei03@daojia-inc.com>
2020-12-12 11:25:22 +0800

    feat: interview

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('**********req.cookies.name', req.cookies.rememberme);
+  res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
   res.render('index', { title: 'Express' });
 });
 
