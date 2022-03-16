beaff5ac05011c684f20b61c5034e78cec8177c1
jiangfei03 <jiangfei03@daojia-inc.com>
2022-03-16 18:24:31 +0800

    feat: 优化页面

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var graphql = require('graphql');
+var { graphqlHTTP } = require('express-graphql');
 var schema = require('../lib/schema');
+class Doctor {
+  constructor() {
+    this.name = 'doctor1';
+    this.hosp = 'xiehe';
+  }
+}
 
-router.use(bodyParser.text({type:'application/graphql'}));
+class Person {
+  constructor() {
+    this.id = 123;
+    this.name = 'jf';
+    this.signDoctor = new Doctor();
+  }
+}
 
-router.post('/', function (req, res, next) {
-  graphql.graphql(schema,req.body).then(function(result){
-    res.send(JSON.stringify(result, null, 2));
-  });
-});
+// root 规定了顶层的 API 入口端点
+const root = {
+  getPerson: function () {
+    return new Person();
+  }
+}
+
+router.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 module.exports = router;
